af8cf1b131378579f2ff204dbee2c5e54b89f4a8
dutiz <zaimidaut@gmail.com>
2022-12-05 20:47:46 +0100

    services updated

--- components/ServiceCart.js
+++ components/ServiceCart.js
@@ -3,7 +3,7 @@ import SVG from 'react-inlinesvg'
 export default function ServiceCart({ svg, title, children }) {
   return (
     <div className="lg:col-4 mt-5 lg:mt-0 text-center">
-      <SVG src={`./SVG/${svg}`} className="mx-auto fill-current w-14 h-14" />
+      <SVG src={`./SVG/${svg}`} className="mx-auto  w-14 h-14" />
       <h2 className="mt-5 text-xl">{title}</h2>
       <p className="mt-5 text-gray-400">{children}</p>
     </div>
--- components/sections/ServiceSection.js
+++ components/sections/ServiceSection.js
@@ -6,17 +6,14 @@ export default function ServiceSection() {
   return (
     <div className="container">
       <div className="row my-24">
-        <ServiceCart svg="architecture.svg" title="Business Plan Services">
-          Aenean lacinia bibendum nulla sed consectetur. Integer posuere erat a ante venenatis
-          dapibus posuere velit aliquet.
+        <ServiceCart svg="architecture.svg" title="Planing">
+          We plan and design your homes according to your wishes
         </ServiceCart>
-        <ServiceCart svg="architecture.svg" title="Business Plan Services">
-          Aenean lacinia bibendum nulla sed consectetur. Integer posuere erat a ante venenatis
-          dapibus posuere velit aliquet.
+        <ServiceCart svg="service-recycle.svg" title="Recycling">
+          Put waste in the right place , for keeping our City clean and green
         </ServiceCart>
-        <ServiceCart svg="architecture.svg" title="Business Plan Services">
-          Aenean lacinia bibendum nulla sed consectetur. Integer posuere erat a ante venenatis
-          dapibus posuere velit aliquet.
+        <ServiceCart svg="service-world.svg" title="Delivery">
+          Monitoring and servicing 24/7 , <br /> Delivered around the world
         </ServiceCart>
       </div>
     </div>
