075c7069930866660a1aaaafe2ab6e8d79de64c9
dutiz <zaimidaut@gmail.com>
2022-12-08 01:25:31 +0100

    Products added

--- components/ProductCarts.js
+++ components/ProductCarts.js
@@ -1,20 +1,16 @@
-import React from 'react'
+import Image from 'next/image'
 
-export default function ProductCarts() {
+export default function ProductCarts({ image }) {
   return (
-    <div className="md:col-6 mt-8   ">
-      <div
-        style={{
-          background: `url('/images/product1.png')`,
-          minHeight: '511px',
-        }}
-        className="transition bg-cover bg-center bg-no-repeat overflow-hidden flex items-end hover:shadow-outline"
-      >
-        <div className="bg-gray-900 opacity-70 w-full p-8">
-          <h1 className="font-bold text-2xl text-white">Product1</h1>
-          <p className="font-medium text-white">Dimensions: 400 m2 , Location: Switzerland, </p>
-        </div>
-      </div>
+    <div className="md:col-6 relative">
+      <Image
+        src={image}
+        width={438}
+        height={400}
+        className="w-full h-full object-cover"
+        alt="products"
+        style={{ maxHeight: '400px' }}
+      />
     </div>
   )
 }
--- pages/products.js
+++ pages/products.js
@@ -1,14 +1,31 @@
+import { useState } from 'react'
+
+import Button from '@/components/Button'
 import ProductCarts from '@/components/ProductCarts'
 import QuotationSection from '@/components/sections/QuotationSection'
 
 import Layout2 from '../components/layout/Layout2'
+import items from '../content/products'
 
-export default function products() {
+export default function Products() {
+  const [category, setCategory] = useState('Stairs')
+  const categories = items.items.find((i) => i.name === category)
   return (
     <Layout2 title="Products">
       <div className="container">
-        <div className="row">
-          <ProductCarts />
+        <div className="row my-6">
+          <div className="grid gap-4 lg:grid-cols-5 md:grid-cols-4 md:gap-8">
+            <Button onClick={() => setCategory('House')}>House</Button>
+            <Button onClick={() => setCategory('Stairs')}>Stairs</Button>
+            <Button onClick={() => setCategory('Decorations')}>Decorations</Button>
+            <Button onClick={() => setCategory('Smart Parking')}>Smart Parking</Button>
+            <Button onClick={() => setCategory('Smart Containers')}>Smart Containers</Button>
+          </div>
+        </div>
+        <div className="row g-0 my-8">
+          {categories.items.map((item) => (
+            <ProductCarts key={item.name} image={item.image}></ProductCarts>
+          ))}
         </div>
       </div>
       <QuotationSection />
