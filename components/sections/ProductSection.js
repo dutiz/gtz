06ccf44b05ce72a6738a13e2f97818018f7c0dd4
dutiz <zaimidaut@gmail.com>
2022-12-11 00:56:21 +0100

    products update

--- components/sections/ProductSection.js
+++ components/sections/ProductSection.js
@@ -6,7 +6,9 @@ export default function ProductSection() {
       <div className="row g-0 my-12">
         <ProductCart image="/images/products/houses/house3-4.jpeg">House</ProductCart>
         <ProductCart image="/images/products/stairs/stairs-5.jpeg">Stairs</ProductCart>
-        <ProductCart image="/images/smart-container.jpeg">Smart Containers</ProductCart>
+        <ProductCart image="/images/products/smart-containers/container-3.jpg">
+          Smart Containers
+        </ProductCart>
       </div>
     </div>
   )
