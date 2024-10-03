5b0b26762f9575cbf55822248aa8f416da8aefbe
EmirJ1 <116313941+EmirJ1@users.noreply.github.com>
2024-10-04 00:03:48 +0200

    added extra informations to teamcart

--- components/TeamCart.js
+++ components/TeamCart.js
@@ -1,7 +1,33 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import Image from 'next/image'
+import { useState } from 'react'
 
-export default function TeamCart({ avatar, position, children }) {
-  return (
+export default function TeamCart({ avatar, position, vertrieb, adress, phonenr, email, children }) {
+  const [clicked, useClicked] = useState(false)
+
+  return vertrieb ? (
+    <div className="md:col-6 mt-8">
+      <div className="flex flex-row items-center border border-gray-50">
+        <div className="col-6">
+          <Image src={`/images/${avatar}`} width={198} height={264} alt="team1" />
+        </div>
+        <div className="col-6 pl-3 md:pl-0">
+          <p>{children}</p>
+          <p className="mt-3 text-gray-700">{position}</p>
+          {clicked ? (
+            <>
+              <p className="mt-5">{adress} </p>
+              <p>Mail: {email} </p>
+              <p>Phone Number: {phonenr} </p>
+              <button onClick={() => useClicked(false)}>Show Less</button>
+            </>
+          ) : (
+            <button onClick={() => useClicked(true)}>Show More</button>
+          )}
+        </div>
+      </div>
+    </div>
+  ) : (
     <div className="md:col-6 mt-8">
       <div className="flex flex-row items-center border border-gray-50">
         <div className="col-6">
